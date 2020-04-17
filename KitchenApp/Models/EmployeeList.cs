89d1928a7cbde654236b7873294fa81efc16c944
z0bot <z_series@protonmail.ch>
2020-04-16 20:41:23 -0500

    Added employee request, post request and changed UI

---
+++ KitchenApp/Models/EmployeeList.cs
@@ -0,0 +1,14 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenApp.Models
+{
+    public class EmployeeList : RealmObject
+    {
+        public IList<Employee> employees { get; }
+    }
+}
---
+++ KitchenApp/Models/Requests/GetEmployeeListRequest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace KitchenApp.Models.Requests
+{
+    public class GetEmployeeListRequest : ServiceRequest
+    {
+        public override string Url => "https://dijkstras-steakhouse-restapi.herokuapp.com/employees";
+        public override HttpMethod Method => HttpMethod.Get;
+        public override Dictionary<string, string> Headers => null;
+
+        public static async Task<bool> SendGetEmployeeListRequest()
+        {
+            var sendGetEmployeeListRequest = new GetEmployeeListRequest();
+            var response = await ServiceRequestHandler.MakeServiceCall<EmployeeList>(sendGetEmployeeListRequest);
+
+            if (response != null)
+            {
+                RealmManager.AddOrUpdate<EmployeeList>(response);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
